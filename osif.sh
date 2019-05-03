665e522bec8e7d8e8098273ef6273d799bb4c078
zzpay1110 <50121531+zzpay1110@users.noreply.github.com>
2019-05-03 11:40:01 +0700

    Add files via upload

---
+++ osif.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/bash
+clear
+echo "";
+read -p "@OSIF******Silahkan Enter Untuk Menginstall Bahan :) " a;
+sleep 2
+echo " *******Installer by @zzpay*******"
+sleep 2
+echo "[+] Tunggu Sebentar Sedang Menginstall PYTHON2...";
+pkg install python2
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall GIT...";
+pkg install git
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall REQUESTS...";
+pip2 install requests
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall MECHANIZE...";
+pip2 install mechanize
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall OSIF...";
+unzip osif.zip -o
+sleep 1
+echo "[+] Installing...";
+pip2 install -r requirements.txt
+sleep 2
+echo "";
+echo "[+] Install Selesai...";
+sleep
+sh start.sh
